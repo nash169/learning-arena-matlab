4922e88df1f272210e61b220875c429265699736
Bernardo Fichera <bernardo.fichera@gmail.com>
2019-06-24 10:17:12 +0200

    In sampling DS added flag to check the reaching of the target

--- dynamics/SampleDS.m
+++ dynamics/SampleDS.m
@@ -1,4 +1,4 @@
-function [x_dot, x_next] = SampleDS(f, x_0, dt, T)
+function [x_dot, x_next] = SampleDS(f, x_0, x_T, dt, T)
 
 % Create storage vectors in order to accept different formats
 if size(x_0,1) == 1
@@ -12,6 +12,10 @@ if size(x_0,1) == 1
         x_next(i,:) = x_next(i-1,:) + dt*x_dot(i-1,:);
         x_dot(i,:) = f(x_next(i,:));
     end
+    
+    if x_next(end,:) - x_T >= 0.01
+        error('Target not reached');
+    end
 else
     x_dot = zeros(length(x_0),T/dt);
     x_next = x_dot;
@@ -22,6 +26,10 @@ else
     for i = 2:T/dt+1
         x_next(:,i) = x_next(:,i-1) + dt*x_dot(:,i-1);
         x_dot(:,i) = f(x_next(:,i));
+    end
+    
+    if x_next(:,end) - x_T >= 0.01
+        error('Target not reached');
     end
 end
 
